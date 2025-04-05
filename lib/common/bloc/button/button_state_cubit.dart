ff0361223da708e0627a170e3e550563442b5e8f
Himanshu-sh0 <buildforev@gmail.com>
2025-04-05 08:09:07 +0530

    Implement Cubit for managing  Button states

---
+++ lib/common/bloc/button/button_state_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:truck_management_app/common/bloc/button/button_state.dart';
+import 'package:truck_management_app/core/usecases/usecase.dart';
+
+class ButtonStateCubit extends Cubit<ButtonState> {
+  ButtonStateCubit() : super(ButtonInitialState());
+
+  void execute({dynamic params, required Usecase usecase}) async {
+    emit(ButtonLoadingState());
+    await Future<void>.delayed(const Duration(seconds: 2));
+    try {
+      final result = await usecase.call(params);
+      result.fold(
+        (failure) {
+          emit(ButtonFailureState(errorMessage: failure.message));
+        },
+        (success) {
+          emit(ButtonSuccessState());
+        },
+      );
+    } catch (e) {
+      emit(ButtonFailureState(errorMessage: e.toString()));
+    }
+  }
+}
