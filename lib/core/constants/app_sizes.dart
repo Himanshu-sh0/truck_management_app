8bbfe85ed2783b33f65c6b1f767409d54550562e
Himanshu-sh0 <buildforev@gmail.com>
2025-04-09 22:27:50 +0530

    Add more padding size

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -2,6 +2,7 @@ class AppSizes {
   // Padding and margin sizes
   static const double paddingXS = 4.0;
   static const double paddingS = 8.0;
+  static const double paddingSL = 10.0;
   static const double paddingM = 16.0;
   static const double paddingL = 24.0;
   static const double paddingXL = 32.0;
